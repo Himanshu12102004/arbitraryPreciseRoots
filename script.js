7d36c4b7fe11e03ee1f5fdd4bb98a039b661f9f4
himanshu gupta <himanshugupta@himanshus-MacBook-Air.local>
2023-07-22 02:40:46 +0530

    prject finally done

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 const answer = document.getElementById("answer");
 const wait = document.getElementById("wait");
+let isNegative = false;
 answer.style.maxWidth = innerWidth - 30 + "px";
 function checkThePlace(a, b) {
   return a !== b;
 }
 function calulateRoot() {
+  isNegative = false;
+
   answer.style.display = "block";
 
   document.body.style.overflow = "hidden";
   wait.style.display = "flex";
   let previousNo = "";
-  const n = document.getElementById("number").value;
-  let myNo = new Decimal(n);
+  let n = document.getElementById("number").value;
+
   const pow = document.getElementById("pow").value;
+  if (n < 0 && pow % 2 === 1) {
+    isNegative = true;
+    n = n * -1;
+  }
+  if (n < 0 && pow % 2 === -1) {
+    isNegative = true;
+    n = n * -1;
+  }
+
+  let myNo = new Decimal(n);
   let myPower = new Decimal(pow);
   const decimalDigits = parseInt(document.getElementById("decimal").value);
+  if (decimalDigits < 0) {
+    wait.style.display = "none";
+    document.body.style.overflow = "auto";
+    answer.innerHTML = `<div style="color:red; border-color:red">Number of decimal digits cannot be negative</div>`;
+    return;
+  }
   let presentNo = new Decimal(Math.pow(n, 1 / pow));
   if (parseFloat(presentNo.toString()) % 1 === 0) {
-    answer.innerHTML =
-      `<div style="color:#ffe700;display:inline-block">Your Answer for ${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
-      presentNo.toFixed(decimalDigits).toString();
+    if (isNegative) {
+      answer.innerHTML =
+        `<div style="color:#ffe700;display:inline-block">Your Answer for -${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
+        "-" +
+        presentNo.toFixed(decimalDigits).toString();
+    } else
+      answer.innerHTML =
+        `<div style="color:#ffe700;display:inline-block">Your Answer for ${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
+        presentNo.toFixed(decimalDigits).toString();
     wait.style.display = "none";
     document.body.style.overflow = "auto";
 
     return;
   }
   let one = new Decimal(1);
-  Decimal.set({ precision: decimalDigits + 1 });
+  Decimal.set({ precision: decimalDigits + 1 + 5 });
 
   function performIteration(i) {
-    console.log(i);
     let powMinusOne = myPower.minus(one);
     let reciprocalOfPower = one.div(myPower);
     let raisedToPower = presentNo.pow(powMinusOne);
@@ -38,18 +62,29 @@ function calulateRoot() {
     let sum = product.plus(fraction);
     previousNo = presentNo;
     presentNo = reciprocalOfPower.times(sum);
-    answer.innerHTML =
-      `<div style="color:#ffe700;display:inline-block">Your Answer for ${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
-      presentNo.toString();
-
+    if (isNegative) {
+      answer.innerHTML =
+        `<div style="color:#ffe700;display:inline-block">Your Answer for -${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
+        "-" +
+        presentNo.toFixed(decimalDigits).toString();
+    } else
+      answer.innerHTML =
+        `<div style="color:#ffe700;display:inline-block">Your Answer for ${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
+        presentNo.toFixed(decimalDigits).toString();
     if (checkThePlace(previousNo.toString(), presentNo.toString())) {
       setTimeout(() => performIteration(i + 1), 0);
     } else {
       wait.style.display = "none";
       document.body.style.overflow = "auto";
-      answer.innerHTML =
-        `<div style="color:#ffe700;display:inline-block">Your Answer for ${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
-        presentNo.toString();
+      if (isNegative) {
+        answer.innerHTML =
+          `<div style="color:#ffe700;display:inline-block">Your Answer for -${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
+          "-" +
+          presentNo.toFixed(decimalDigits).toString();
+      } else
+        answer.innerHTML =
+          `<div style="color:#ffe700;display:inline-block">Your Answer for ${n}<sup style="font-size:10px">1/${pow}</sup>=</div>` +
+          presentNo.toFixed(decimalDigits).toString();
     }
   }
 
